413cace70c291a11b4f50ada2a5fac4950d53db8
Dias Ideias <ctrldiasideias@gmail.com>
2019-04-29 15:19:54 -0300

    Versão Layout II

--- frontend/src/component/template/Footer.jsx
+++ frontend/src/component/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por  
+            <strong><span className="text-success"> dias</span> <span className="text-ideias">ideias</span></strong>
+        </span>
     </footer>
