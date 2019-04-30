6f4b73ec92f56959dadb058a221cafedffb4035f
Dias Ideias <ctrldiasideias@gmail.com>
2019-04-30 12:34:29 -0300

    Finalização do Crud s/ responsividade

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../component/template/Logo'
 import Nav from '../component/template/Nav'
@@ -10,11 +10,11 @@ import Routes from './Routes'
 import Footer from '../component/template/Footer'
 
 export default props =>
-    <HashRouter>
+    <BrowserRouter>
         <div className="app">
             <Logo />
             <Nav />
             <Routes />
             <Footer />
         </div>
-    </HashRouter>
+    </BrowserRouter>
